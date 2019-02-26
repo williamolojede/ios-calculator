c2f6b323ddf28ce88912890417bb40dfd88a54ed
William I. Olojede <ilerioluwase97@gmail.com>
2019-02-27 00:55:58 +0100

    Updates

---
+++ precache-manifest.fcba771b614e034ab8c7ca130cd5a249.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ca14d969f5e8c8a1b3da",
+    "url": "/ios-calculator/static/js/runtime~main.ca14d969.js"
+  },
+  {
+    "revision": "c7a70feff147c1d75a83",
+    "url": "/ios-calculator/static/js/main.c7a70fef.chunk.js"
+  },
+  {
+    "revision": "f4d10aee93265461729a",
+    "url": "/ios-calculator/static/js/1.f4d10aee.chunk.js"
+  },
+  {
+    "revision": "c7a70feff147c1d75a83",
+    "url": "/ios-calculator/static/css/main.7e7a6400.chunk.css"
+  },
+  {
+    "revision": "17de150759777ccb26857bd21e5f5c55",
+    "url": "/ios-calculator/index.html"
+  }
+];
